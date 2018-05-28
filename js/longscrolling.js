95865ff216ef18b33cdabc8bd5f80c1e1415ef21
lanlan48 <37819139+lanlan48@users.noreply.github.com>
2018-05-28 15:33:39 -0700

    sdfdsf

---
+++ js/longscrolling.js
@@ -0,0 +1,43 @@
+var canvas = document.getElementById("canvas");
+var counter = document.getElementById("counter");
+var icon = document.getElementById("icon");
+
+window.onscroll = function(){
+
+
+  var y = window.pageYOffset;
+
+  console.log(y);
+
+  counter.style.top = 400 + y * .97 + "px";
+
+  counter.innerHTML = y + "px";
+
+  if (y > 2000) {
+    counter.style.backgroundColor = "green";
+  }else{
+    counter.style.backgroundColor = "blue";}
+
+    icon.style.top = 200 + y  + "px";
+
+  if(y < 2000){
+
+      icon.src="meteor.png";
+
+  }
+
+
+  else if(y < 9000){
+
+      icon.src="second.png";
+
+  }
+
+
+
+  else{
+
+  icon.src="three.png";
+ }
+
+};
